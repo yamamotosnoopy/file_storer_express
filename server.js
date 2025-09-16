fc8e9c4d72f29d504755ff041f72e5da7569ae91
Darian Morales <dtmorales77@gmail.com>
2025-09-17 00:51:07 +0900

    Added basic home page and minor scaffolding for user authentication.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
+
 const usersRouter = require('./routes/users');
 
+app.use(express.static("public"));
 
 app.get("/", (req, res) => res.send("Hello, World!"));
 app.use('/users', usersRouter);
